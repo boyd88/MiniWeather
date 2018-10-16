b31f14e87ece4ad9e698449349c2f983248e2890
augustus <kingszeto@163.com>
2018-10-16 19:46:34 +0800

    charpter4

--- app/src/main/java/com/example/stby/miniweather/MainActivity.java
+++ app/src/main/java/com/example/stby/miniweather/MainActivity.java
@@ -2,11 +2,24 @@ package com.example.stby.miniweather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import stby.util.NetUtil;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+
+        if(NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE){
+            Log.d("myWeather","网络OK");
+            Toast.makeText(MainActivity.this, "网络OK!", Toast.LENGTH_SHORT).show();
+        }else
+        {
+            Log.d("myWeather","网络挂了");
+            Toast.makeText(MainActivity.this,"网络挂了",Toast.LENGTH_LONG).show();
+        }
     }
 }
