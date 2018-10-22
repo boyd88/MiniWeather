847e9c0b615be48d41af859b582d26b67b2d51c7
augustus <kingszeto@163.com>
2018-10-22 19:11:34 +0800

    charpter5

--- app/src/main/java/com/example/stby/miniweather/MainActivity.java
+++ app/src/main/java/com/example/stby/miniweather/MainActivity.java
@@ -1,17 +1,31 @@
 package com.example.stby.miniweather;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import stby.util.NetUtil;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
+    private ImageView mUpdateBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+
+        mUpdateBtn = (ImageView) findViewById(R.id.title_update_btn);
+        mUpdateBtn.setOnClickListener(this);
 
         if(NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE){
             Log.d("myWeather","网络OK");
@@ -21,5 +35,55 @@ public class MainActivity extends Activity {
             Log.d("myWeather","网络挂了");
             Toast.makeText(MainActivity.this,"网络挂了",Toast.LENGTH_LONG).show();
         }
+    }
+
+    @Override
+    public void onClick(View view){
+        if(view.getId() == R.id.title_update_btn){
+
+            SharedPreferences sharedPreferences =getSharedPreferences("config",MODE_PRIVATE);
+            String citycode = sharedPreferences.getString("main_city_code","101010100");
+        } else{
+            Log.d("myWeather","网络挂了");
+            Toast.makeText(MainActivity.this,"网络挂了",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     *
+     * @param citycode
+     */
+    private void queryWeatherCode(String citycode){
+        final String address = "http://wthrcdn.etouch.cn/WeatherApi?citykey=" + citycode;
+        Log.d("myWeather",address);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection con = null;
+                try{
+                    URL url = new URL(address);
+                    con = (HttpURLConnection)url.openConnection();
+                    con.setRequestMethod("GET");
+                    con.setConnectTimeout(8000);
+                    con.setReadTimeout(8000);
+                    InputStream in = con.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String str;
+                    while((str = reader.readLine()) != null){
+                        response.append(str);
+                        Log.d("myWeather", str);
+                    }
+                    String responseStr=response.toString();
+                    Log.d("myWeather", responseStr);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    if(con != null){
+                        con.disconnect();
+                    }
+                }
+            }
+        }).start();
     }
 }
